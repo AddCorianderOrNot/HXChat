b50dea7ad3925a00d221af7bc0dd5f0015acb6bd
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-11-27 21:48:33 +0800

    实现词云网络接口
    TODO 词云呈现

--- app/src/main/java/com/example/hxchat/viewmodel/request/RequestWordCloudViewModel.kt
+++ app/src/main/java/com/example/hxchat/viewmodel/request/RequestWordCloudViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.hxchat.viewmodel.request
 
 import androidx.lifecycle.MutableLiveData
+import com.example.hxchat.R
+import com.example.hxchat.app.ext.nav
+import com.example.hxchat.app.ext.showMessage
+import com.example.hxchat.app.network.apiService
+import com.example.hxchat.app.util.CacheUtil
 import com.example.hxchat.data.model.bean.User
 import me.hgj.jetpackmvvm.base.viewmodel.BaseViewModel
+import me.hgj.jetpackmvvm.ext.navigateAction
+import me.hgj.jetpackmvvm.ext.parseState
+import me.hgj.jetpackmvvm.ext.request
 import me.hgj.jetpackmvvm.state.ResultState
 import net.alhazmy13.wordcloud.WordCloud
 import kotlin.random.Random
@@ -15,11 +23,18 @@ import kotlin.random.Random
 
 class RequestWordCloudViewModel: BaseViewModel(){
     var wordCloud : MutableLiveData<ResultState<ArrayList<WordCloud>>> = MutableLiveData()
+    var word : MutableLiveData<ResultState<ArrayList<String>>> = MutableLiveData()
 
     //发送请求词云的请求
     fun get_wordCloud(){
+        request(
+            { apiService.getWordcloud() },
+            word
+        )
+
         val list:ArrayList<WordCloud> = ArrayList()
         val random = Random(123)
+
         list.add(WordCloud("1", random.nextInt(50)))//第一个字符串是显示的词云的信息，第二个weight是相对大小 数值越大 现实的字体越大
         list.add(WordCloud("fg", random.nextInt(50)))//第一个字符串是显示的词云的信息，第二个weight是相对大小 数值越大 现实的字体越大
         list.add(WordCloud("sadwe", random.nextInt(50)))//第一个字符串是显示的词云的信息，第二个weight是相对大小 数值越大 现实的字体越大
